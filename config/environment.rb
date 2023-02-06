7226d2635d3068028c24ecfeab8717a08edcb7be
Walter <walter.clark24@yahoo.com>
2023-02-06 15:46:16 -0500

    commit tree 5cb559b727c9110892ac58692e1d35f21a09dbed

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,11 @@ Bundler.require
 require_relative "../lib/student"
 
 DB = { conn: SQLite3::Database.new("db/students.db") }
+
+namespace :db do
+desc 'migrate changes to your database'
+task migrate: environment do
+    require_relative ./config/environment
+    Student.create_table
+end
+end
